eaa0eb301e824c8b7d1ac8ab9cd8bd51a523df84
tup32132 <104232100+tup32132@users.noreply.github.com>
2023-01-07 17:42:59 -0500

    create complement machine, eggs machine

---
+++ complementMachine.c
@@ -0,0 +1,10 @@
+#include <stdio.h> 
+
+int main ()
+{
+	printf("Hello, what is your name?\n");
+	char name [31];
+	scanf("%s", name);	
+       	printf("Hello %s\nYou look sexy as fuck tonight.\n", name); 
+	 return 0;
+}
